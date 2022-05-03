f02ea3fce6d2430c2953d589057271e30e9ea30e
Sahar Mirkamrani <shrmrkmrn@gmail.com>
2022-05-03 15:40:47 +0430

    1. Smoke test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 <0.9.0;
+
+contract DappToken {
+	//constructor
+	//set the total number of tokens
+	//read the total number of tokens
+
+	uint public totalSupply = 100;
+
+	function dappToken() public {
+		totalSupply = 100;
+	}
+}
